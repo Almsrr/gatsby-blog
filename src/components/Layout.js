73b5a6c48b9b7db8a204e342af5a6f306368bb98
Alam <alam.15@outlook.com>
2021-10-30 13:56:27 -0400

    Finished Gatsby Tutorial Part 2

---
+++ src/components/Layout.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+import { Link } from "gatsby";
+import {
+  container,
+  heading,
+  navLinks,
+  navLinkItem,
+  navLinkText,
+} from "./layout.module.css";
+
+function Layout({ pageTitle, children }) {
+  return (
+    <div className={container}>
+      <title>{pageTitle}</title>
+      <header>
+        <nav>
+          <ul className={navLinks}>
+            <li className={navLinkItem}>
+              <Link to="/" className={navLinkText}>
+                Home
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link to="/about" className={navLinkText}>
+                About
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <h1 className={heading}>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  );
+}
+
+export default Layout;
---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Layout from "../components/Layout";
+
+function AboutPage() {
+  return (
+    <Layout pageTitle="About">
+      <p>I'm the best web developer</p>
+    </Layout>
+  );
+}
+
+export default AboutPage;
