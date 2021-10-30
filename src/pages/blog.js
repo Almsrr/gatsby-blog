fa4a40377e72a690e11457697e57af1e8d78e392
Alam <alam.15@outlook.com>
2021-10-30 17:55:14 -0400

    fix blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 
@@ -23,7 +23,7 @@ export const query = graphql`
     allMdx(sort: { fields: frontmatter___date, order: DESC }) {
       nodes {
         frontmatter {
-          date(formatString: "MMMM D, YYYY")
+          date(formatString: "MMMM DD, YYYY")
           title
         }
         id
