e792a8ceed41b679ebe27a9c0ea04f1256418351
Alam <alam.15@outlook.com>
2021-10-30 14:13:36 -0400

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 import Layout from "../components/Layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 function IndexPage() {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        src="https://www.rover.com/blog/wp-content/uploads/2018/12/dog-sneeze-1-1024x945.jpg"
+        alt="smiling-dog"
+      />
+      <StaticImage src="../images/dog.jpeg" alt="black-and-white-dog" />
     </Layout>
   );
 }
