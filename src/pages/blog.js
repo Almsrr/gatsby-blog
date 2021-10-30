f9974d7d918fb3886335d1e38554f3200d026528
Alam <alam.15@outlook.com>
2021-10-30 15:36:08 -0400

    Finished Gatsby Tutorial Part 4

---
+++ src/pages/blog.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import Layout from "../components/layout";
+import { graphql } from "gatsby";
+
+function BlogPage({ data }) {
+  return (
+    <Layout pageTitle="My Blog Posts">
+      <p>My cool posts will go in here</p>
+      <ul>
+        {data.allFile.nodes.map((node) => (
+          <li key={node.name}>{node.name}</li>
+        ))}
+      </ul>
+    </Layout>
+  );
+}
+
+export const query = graphql`
+  query {
+    allFile(filter: { sourceInstanceName: { eq: "blog" } }) {
+      nodes {
+        name
+      }
+    }
+  }
+`;
+
+export default BlogPage;
